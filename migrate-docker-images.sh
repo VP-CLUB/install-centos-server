1e4f021b2d6865c681c4cfb67a3432dd7bceeffd
John Deng <deng.binghan@vpclub.cn>
2016-12-05 01:39:31 -0500

    update migrate docker images

--- migrate-docker-images.sh
+++ migrate-docker-images.sh
@@ -1,16 +1,60 @@
-#/bin/bash
+#!/bin/bash
 
-source log.sh
-source clap.sh
+source "log.sh"
+source "clap.sh"
 
-echo $(docker images | grep $src) | { while read row; do
-	
-	docker_image=$(echo $row | awk -v N=1 '{print $N}')
-	docker_tag=$(echo $row | awk -v N=2 '{print $N}')
+dc=/usr/bin/docker
 
-	docker tag $docker_image:$docker_tag $dest/$docker_image:$docker_tag
+if [ "$config" == "" ]; then
+  config=./config/docker-images
+fi
 
-	docker push $dest/$docker_image:$docker_tag
-        
-    done
-} 
+filter_args=""
+if [ "$filter" != "" ]; then
+  filter_args="| grep $filter"
+fi
+
+if [ "$all" == "all" ]; then
+  pull=pull
+  tag=tag
+  push=push
+fi
+
+cat $config $filter_args | { while read row; do
+
+  image_name=$(echo $row | awk -v N=1 '{print $N}')
+  image_id=$(echo $row | awk -v N=3 '{print $N}')
+  tag=$(echo $row | awk -v N=2 '{print $N}')
+  tag=${tag/\<none\>/}
+  if [ "$tag" == "" ]; then
+    image_full_name=$image_name
+  else
+    image_full_name=$image_name:$tag
+  fi
+
+  if [ "$pull" == "pull" ]; then
+    proxychains4 $dc pull $image_full_name
+    if [ "$only" == "only" ]; then
+      continue
+    fi
+  fi
+
+  if [ "$tag" == "tag" ]; then
+    $dc tag $image_id $image_full_name 
+    if [ "$only" == "only" ]; then
+      continue
+    fi
+  fi
+
+  if [ "$push" == "push" ]; then
+    $dc push $new_image
+    if [ "$only" == "only" ]; then
+      continue
+    fi
+  fi
+
+
+done }
+
+log "Done"
+
